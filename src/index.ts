57eaf2458735c2cfde5af365b43ed2137630809f
khannedy <echo.khannedy@gmail.com>
2024-10-31 17:21:07 +0700

    Add new API endpoints and sub-route handling
    Introduced new GET and POST endpoints for enhanced API functionality. Added a nested `/api/book` sub-route with multiple endpoints to modularize and organize route handling better. Created a test.http file for HTTP test requests.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import {Hono} from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Programmer Zaman Now!')
+app
+    .get('/hello/:name', (c) => {
+        const name = c.req.param('name')
+        return c.text(`Hello ${name}`)
+    })
+    .post('/post', (c) => {
+        return c.text("Post Hello")
+    })
+    .get('/products/:id{[0-9]+}', async (c) => {
+        const id = c.req.param("id")
+        return c.text(`Product ${id}`)
+    })
+    .get('/', (c) => {
+        return c.text('Hello Programmer Zaman Now!')
+    })
+
+const book = new Hono().basePath('/api');
+book.get('/book', (c) => {
+    return c.text("Book")
 })
+book.get('/book/a', (c) => {
+    return c.text("Book A")
+})
+book.get('/book/:id', (c) => {
+    return c.text("Book Id")
+})
+
+
+app.route('/', book)
 
 export default app
